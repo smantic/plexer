7fff0280d787c448da399f89e8bb72a638158d3f
smantic <tyler@smantic.dev>
2021-12-11 22:37:03 -0600

    add diskspace and queue details radarr requests

---
+++ pkg/radarr/diskspace.go
@@ -0,0 +1,48 @@
+package radarr
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// DiskSpace is the response we get from radarr disk space endpoint
+// {
+//    "path": "D:\\",
+//    "label": "DrivePool",
+//    "freeSpace": 16187217043456,
+//    "totalSpace": 56009755148288
+//  },
+type DiskSpace struct {
+	Path       string `json:"path"`
+	Label      string `json:"label"`
+	FreeSpace  int    `json:"freeSpace"`
+	TotalSpace int    `json:"totalSpace"`
+}
+
+func (c *Client) DiskSpace(ctx context.Context) ([]DiskSpace, error) {
+
+	result := []DiskSpace{}
+
+	url := fmt.Sprintf("%s/diskspace", c.BaseURL)
+	r, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return result, err
+	}
+
+	r.Header.Add("Accept", "application/json")
+	r.Header.Add("X-Api-key", c.Apikey)
+
+	response, err := c.Http.Do(r)
+	if err != nil {
+		return result, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&result)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
